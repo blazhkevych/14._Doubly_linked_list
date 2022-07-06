476ce14787a773a78a72b132a2c605f4ee42746e
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-07-06 22:03:43 +0300

    Add project files.

---
+++ task/List.cpp
@@ -0,0 +1,243 @@
+﻿#include <iostream>
+#include "List.h"
+
+using namespace std;
+
+List::List()
+{
+	Head = Tail = nullptr;
+	Count = 0;
+}
+
+List::List(const List& L)
+{
+	Head = Tail = nullptr;
+	Count = 0;
+	Elem* temp = L.Head;
+	while (temp != nullptr)
+	{
+		AddTail(temp->data);
+		temp = temp->next;
+	}
+}
+
+List::~List()
+{
+	DelAll();
+}
+
+void List::AddHead(char data)
+{
+	Elem* temp = new Elem;
+	temp->prev = nullptr;
+	temp->data = data;
+	temp->next = Head;
+	if (Head != nullptr)
+		Head->prev = temp;
+	Head = temp;
+	if (Tail == nullptr)
+		Tail = temp;
+	Count++;
+}
+
+void List::AddTail(char data)
+{
+	Elem* temp = new Elem;
+	temp->next = nullptr;
+	temp->data = data;
+	temp->prev = Tail;
+	if (Tail != nullptr)
+		Tail->next = temp;
+	Tail = temp;
+	if (Head == nullptr)
+		Head = temp;
+	Count++;
+}
+
+void List::DelHead()
+{
+	if (Head != nullptr)
+	{
+		Elem* temp = Head;
+		Head = Head->next;
+		if (Head != nullptr)
+			Head->prev = nullptr;
+		else
+			Tail = nullptr;
+		delete temp;
+		Count--;
+	}
+}
+
+void List::DelTail()
+{
+	if (Tail != nullptr)
+	{
+		Elem* temp = Tail;
+		Tail = Tail->prev;
+		if (Tail != nullptr)
+			Tail->next = nullptr;
+		else
+			Head = nullptr;
+		delete temp;
+		Count--;
+	}
+}
+
+void List::Insert(char data, int pos)
+{
+	if (pos < 0 || pos > Count)
+		return;
+	if (pos == 0)
+	{
+		AddHead(data);
+		return;
+	}
+	if (pos == Count)
+	{
+		AddTail(data);
+		return;
+	}
+	Elem* current = nullptr;
+	if (pos < Count / 2)
+	{
+		current = Head;
+		int i = 0;
+		while (i < pos)
+		{
+			current = current->next;
+			i++;
+		}
+	}
+	else
+	{
+		current = Tail;
+		int i = Count - 1;
+		while (i > pos)
+		{
+			current = current->prev;
+			i--;
+		}
+	}
+	Elem* temp = new Elem;
+	temp->data = data;
+	temp->prev = current->prev;
+	temp->next = current;
+	current->prev->next = temp;
+	current->prev = temp;
+	Count++;
+}
+
+void List::Del(int pos)
+{
+	if (pos < 0 || pos >= Count)
+		return;
+	if (pos == 0)
+	{
+		DelHead();
+		return;
+	}
+	if (pos == Count - 1)
+	{
+		DelTail();
+		return;
+	}
+	Elem* delElement = nullptr;
+	if (pos < Count / 2)
+	{
+		delElement = Head;
+		int i = 0;
+		while (i < pos)
+		{
+			delElement = delElement->next;
+			i++;
+		}
+	}
+	else
+	{
+		delElement = Tail;
+		int i = Count - 1;
+		while (i > pos)
+		{
+			delElement = delElement->prev;
+			i--;
+		}
+	}
+	delElement->prev->next = delElement->next;
+	delElement->next->prev = delElement->prev;
+	delete delElement;
+	Count--;
+}
+
+void List::PrintHead()
+{
+	Elem* current = Head;
+	while (current != nullptr)
+	{
+		cout << current->data;
+		current = current->next;
+	}
+	cout << endl;
+}
+
+void List::PrintTail()
+{
+	Elem* current = Tail;
+	while (current != nullptr)
+	{
+		cout << current->data;
+		current = current->prev;
+	}
+	cout << endl;
+}
+
+void List::DelAll()
+{
+	while (Head != nullptr)
+		DelHead();
+}
+
+int List::GetCount()
+{
+	return Count;
+}
+
+Elem* List::GetElem(int pos)
+{
+	Elem* temp = Head;
+	if (pos < 0 || pos >= Count)
+		return nullptr;
+	int i = 0;
+	while (i < pos)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return temp;
+}
+
+List& List::operator = (const List& L)
+{
+	if (this == &L)
+		return *this;
+	DelAll();
+	Elem* temp = L.Head;
+	while (temp != nullptr)
+	{
+		AddTail(temp->data);
+		temp = temp->next;
+	}
+	return *this;
+}
+
+// Метод поиска заданного элемента по ключу. Ключ
+// передаётся методу в качестве параметра. Метод
+// возвращает позицию найденного элемента в случае
+// успеха или - 1 в противном случае.
+int List::Find(char key)
+{
+	return 0;
+}
+
+
+
---
+++ task/task.cpp
@@ -0,0 +1,59 @@
+﻿/*
+############################################ ЗАДАНИЕ ###########################################
+					Добавить в класс "Двусвязный список": 
+	 Метод поиска заданного элемента по ключу. Ключ 
+передаётся методу в качестве параметра. Метод 
+возвращает позицию найденного элемента в случае 
+успеха или -1 в противном случае. 
+int Find (char key);
+	 Метод замены значения указанного элемента на другое 
+значение, передаваемое методу в качестве параметра. 
+void Replace (char value, int position);
+	 Метод удаления элементов списка, значение которых 
+совпадает с заданным ключом. 
+void DelElementByKey(char key);
+	 Перегруженный оператор индексирования. 
+char& operator[](int position);
+	 Метод сортировки элементов списка.
+void Sort();
+################################################################################################
+		СТАТУС : Не готово.
+################################################################################################
+*/
+
+#include <iostream>
+#include "List.h"
+
+using namespace std;
+int main()
+{
+	List lst;
+	char s[] = "Hello, World !!!";
+	cout << s << endl;
+	int len = strlen(s);
+	for (int i = len - 1; i >= 0; i--)
+		lst.AddHead(s[i]);
+	cout << "\nFirst list\n";
+	lst.PrintHead();
+	List lst2 = lst;
+	cout << "\nSecond list\n";
+	lst2.PrintHead();
+	List lst3;
+	lst3 = lst;
+	cout << "\nThird list\n";
+	lst3.PrintHead();
+	cout << "\nFirst list\n";
+	lst.PrintTail();
+	lst.Del(2);
+	lst.PrintHead();
+	lst.Insert('?', 0);
+	lst.Insert('?', 4);
+	lst.Insert('?', 4);
+	lst.PrintHead();
+	lst.DelHead();
+	lst.PrintHead();
+	lst.DelTail();
+	lst.PrintHead();
+
+	return 0;
+}
